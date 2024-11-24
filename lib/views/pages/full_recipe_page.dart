54c49c4506d553d0ba0ea7d7a3caa704755dfaaa
Bryeinstein <bryan.dam05@gmail.com>
2024-11-24 20:02:30 +0100

    AddToFavorite button added

--- lib/views/pages/full_recipe_page.dart
+++ lib/views/pages/full_recipe_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../models/recipe.dart';
 import '../widget/full_recipe_card.dart';
+import '../widget/add_fav_button.dart';
 
 class FullRecipePage extends StatelessWidget {
   final Recipe recipe;
@@ -14,8 +15,19 @@ class FullRecipePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: FullRecipeCardWidget(recipe: recipe),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FullRecipeCardWidget(recipe: recipe),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: AddToFavoritesButton(recipe: recipe,)
+            ),
+          ],
+        ),
       ),
     );
   }
+
 }
