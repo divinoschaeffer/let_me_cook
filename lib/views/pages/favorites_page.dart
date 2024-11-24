35e56b7412d208b2515dbcf844d33013f945fb69
bdam <bdam@etu.unistra.fr>
2024-11-24 17:47:26 +0100

    Beginning of the favs recipes page

--- lib/views/pages/favorites_page.dart
+++ lib/views/pages/favorites_page.dart
@@ -5,6 +5,39 @@ class FavoritesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Text(
+              "LET ME COOK !",
+              style: TextStyle(
+                fontSize: 24,
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w900,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05
+          ),
+          const Text(
+            "FAVORITES RECIPES",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.start,
+          )
+          /*Expanded(
+
+          )*/
+        ],
+      ),
+    );
   }
 }
+
