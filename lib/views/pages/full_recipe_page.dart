de9ee23ba361d46bf95b5c4bac8a3a7743e6d790
Bryeinstein <bryan.dam05@gmail.com>
2025-01-04 14:17:09 +0100

    Update: Change from statelesswidget to statefulwidget

--- lib/views/pages/full_recipe_page.dart
+++ lib/views/pages/full_recipe_page.dart
@@ -3,7 +3,7 @@ import '../../models/recipe.dart';
 import '../widget/full_recipe_card.dart';
 import '../widget/add_fav_button.dart';
 
-class FullRecipePage extends StatelessWidget {
+class FullRecipePage extends StatefulWidget {
   final Recipe recipe;
 
   const FullRecipePage({
@@ -12,25 +12,47 @@ class FullRecipePage extends StatelessWidget {
   });
 
   @override
+  _FullRecipePageState createState() => _FullRecipePageState();
+}
+
+class _FullRecipePageState extends State<FullRecipePage> {
+  bool _favoritesChanged = false;
+
+  void _onFavoriteToggled() {
+    setState(() {
+      _favoritesChanged = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            FullRecipeCardWidget(recipe: recipe),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Explore the recipe and don't forget to save it if you like it!",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pop(context, _favoritesChanged);
+        return false;
+      },
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FullRecipeCardWidget(recipe: widget.recipe),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "Explore the recipe and don't forget to save it if you like it!",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
+        ),
+        floatingActionButton: AddToFavoritesButton(
+          recipe: widget.recipe,
+          onFavoriteChanged: _onFavoriteToggled,
         ),
       ),
-      floatingActionButton: AddToFavoritesButton(recipe: recipe),
     );
   }
 }
